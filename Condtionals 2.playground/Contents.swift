b0034ca668f4e5a4dc021d107a11de8f50b07afb
Benjamin Pena <benjaminpena@Benjamins-MacBook-Pro.local>
2016-11-16 22:56:05 -0700

    Added conditionals 2

---
+++ Condtionals 2.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var age = 26
+var account = 100000
+var deadUncle = false
+
+if age >= 45 && account >= 80000 || deadUncle == true {
+    print("I can retire")
+} else {
+    print("I cant retire")
+}
+
+if age >= 25 && account >= 80000 || deadUncle == true {
+    print("I can reitre")
+} else {
+    print("I cant retire")
+}
