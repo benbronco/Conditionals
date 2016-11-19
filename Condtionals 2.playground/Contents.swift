e6d89d5dee2e559ce125f6d622755023b585c85b
Benjamin Pena <benjaminpena@Benjamins-MacBook-Pro.local>
2016-11-18 21:27:40 -0700

    added functions to Conditionals playground

--- Condtionals 2.playground/Contents.swift
+++ Condtionals 2.playground/Contents.swift
@@ -17,3 +17,34 @@ if age >= 25 && account >= 80000 || deadUncle == true {
 } else {
     print("I cant retire")
 }
+
+var income = 65000
+var retirementAge = 65
+var inheritance = false
+
+if retirementAge >= 65 && income >= 60000 || inheritance == false {
+    print("You are able to retire")
+} else if retirementAge >= 50 && income >= 75000 || inheritance == true {
+    print("You are able to retire")
+} else {
+    print("You are not able to retire")
+}
+
+var bankAccount = 1000.50
+var game = 400.99
+
+func canPurchase(amount: Double) -> Bool {
+    if bankAccount >= amount {
+        return true
+    } else {
+        return false
+    }
+}
+
+func makePurchase(amt: Double) {
+    bankAccount -= amt
+    print("You purchased the game and there is \(bankAccount) left in the bank")
+}
+
+canPurchase(amount: game)
+makePurchase(amt: game)
